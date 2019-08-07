017b7c9d5184db195de371ff213cda024c6c0b15
Tyler Turnipseed <tyler@techturnip.us>
2019-08-06 22:24:22 -0400

    implemented POST request to add new user

--- index.js
+++ index.js
@@ -51,6 +51,41 @@ server.get('/api/users/:id', (req, res) => {
     })
 })
 
+// POST - '/api/users' - Creates a user using the
+// information sent inside the request body
+server.post('/api/users', (req, res) => {
+  const newUser = req.body
+
+  // Check if newUser name and bio are neither
+  // 'undefined' nor empty strings
+  if (
+    newUser.name &&
+    newUser.name !== '' &&
+    newUser.bio &&
+    newUser.bio !== ''
+  ) {
+    // If the newUser obj is valid, insert into
+    // the database or catch error
+    db.insert(newUser)
+      .then(createdUser => {
+        res.status(201).json(createdUser)
+      })
+      .catch(err => {
+        res.status(500).json({
+          err: err,
+          errorMessage:
+            'There was an error while saving the user to the database'
+        })
+      })
+  } else {
+    // If the newUser obj is invalid, respond with
+    // status 400 and errorMessage
+    res.status(400).json({
+      errorMessage: 'Please provide name and bio for the user.'
+    })
+  }
+})
+
 // LISTEN
 server.listen(4000, () => {
   console.log('Server is running on port 4000...')

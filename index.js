65f2f8ea15397f3b3e9b74b2da8ab55a02693efc
Tyler Turnipseed <tyler@techturnip.us>
2019-08-06 21:41:42 -0400

    added GET request to base url and '/api/users' which returns an array of users

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // implement your API here
+const express = require('express')
+const db = require('./data/db.js')
+
+const server = express()
+
+// middleware
+server.use(express.json())
+
+// ================================================|
+// REQUEST HANDLERS ===============================|
+// ================================================|
+
+// GET - '/' - Basic handle root url
+server.get('/', (req, res) => {
+  res.send('This is the webapi-i-challenge api')
+})
+
+// GET - '/api/users' - Returns an array of all the
+// user objects contained in the database
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(users => {
+      res.json(users)
+    })
+    .catch(err => {
+      res.status(500).json({
+        err: err,
+        message: 'Failed to get users'
+      })
+    })
+})
+
+// LISTEN
+server.listen(4000, () => {
+  console.log('Server is running on port 4000...')
+})
